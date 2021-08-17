b82f6df9902135af85b594902841dbce9ef6f707
Mariame <mariameconte1234@gmail.com>
2021-08-17 11:17:13 +0000

    tout les dossiers

---
+++ src/main/java/com/big/Pro/ProjetsonApplication.java
@@ -0,0 +1,13 @@
+package com.big.Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetsonApplication.class, args);
+	}
+
+}
